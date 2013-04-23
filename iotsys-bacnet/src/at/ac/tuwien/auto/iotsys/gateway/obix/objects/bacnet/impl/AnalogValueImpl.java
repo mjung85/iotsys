29bd0caf14a337224eae91e26630dd5f91005e6e
markus jung85 <markus.jung85@gmail.com>
2013-04-23 08:27:27 +0200

    Moved BACnet specific mapping contracts to commons project

--- iotsys-bacnet/src/at/ac/tuwien/auto/iotsys/gateway/obix/objects/bacnet/impl/AnalogValueImpl.java
+++ iotsys-bacnet/src/at/ac/tuwien/auto/iotsys/gateway/obix/objects/bacnet/impl/AnalogValueImpl.java
@@ -26,7 +26,7 @@ import obix.Contract;
 import obix.Real;
 import at.ac.tuwien.auto.iotsys.gateway.connectors.bacnet.BACnetConnector;
 import at.ac.tuwien.auto.iotsys.gateway.connectors.bacnet.BacnetDataPointInfo;
-import at.ac.tuwien.auto.iotsys.gateway.obix.objects.bacnet.AnalogValue;
+import at.ac.tuwien.auto.iotsys.gateway.obix.object.bacnet.AnalogValue;
 
 public class AnalogValueImpl extends AnalogBacnetObj implements AnalogValue {
 	protected Real value = new Real(0);
--- iotsys-bacnet/src/at/ac/tuwien/auto/iotsys/gateway/obix/objects/bacnet/impl/BinaryValueImpl.java
+++ iotsys-bacnet/src/at/ac/tuwien/auto/iotsys/gateway/obix/objects/bacnet/impl/BinaryValueImpl.java
@@ -25,7 +25,7 @@ package at.ac.tuwien.auto.iotsys.gateway.obix.objects.bacnet.impl;
 import obix.Contract;
 import at.ac.tuwien.auto.iotsys.gateway.connectors.bacnet.BACnetConnector;
 import at.ac.tuwien.auto.iotsys.gateway.connectors.bacnet.BacnetDataPointInfo;
-import at.ac.tuwien.auto.iotsys.gateway.obix.objects.bacnet.BinaryValue;
+import at.ac.tuwien.auto.iotsys.gateway.obix.object.bacnet.BinaryValue;
 
 public class BinaryValueImpl extends BinaryBacnetObj implements BinaryValue {
 	public BinaryValueImpl(BACnetConnector bacnetConnector, BacnetDataPointInfo dataPointInfo) {
