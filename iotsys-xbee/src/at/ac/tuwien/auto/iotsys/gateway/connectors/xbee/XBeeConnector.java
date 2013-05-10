7c1c3bfec8f948384a4ebfe5119460be2ee1f0a7
markus jung85 <markus.jung85@gmail.com>
2013-05-10 15:14:04 +0200

    Obelix

--- iotsys-xbee/src/at/ac/tuwien/auto/iotsys/gateway/connectors/xbee/XBeeConnector.java
+++ iotsys-xbee/src/at/ac/tuwien/auto/iotsys/gateway/connectors/xbee/XBeeConnector.java
@@ -1,31 +1,27 @@
 package at.ac.tuwien.auto.iotsys.gateway.connectors.xbee;
 
-
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.logging.Logger;
 
 import com.rapplogic.xbee.api.ApiId;
-import com.rapplogic.xbee.api.AtCommand;
 import com.rapplogic.xbee.api.PacketListener;
 import com.rapplogic.xbee.api.XBee;
-import com.rapplogic.xbee.api.XBeeAddress64;
-import com.rapplogic.xbee.api.XBeeException;
-import com.rapplogic.xbee.api.XBeeRequest;
 import com.rapplogic.xbee.api.XBeeResponse;
-import com.rapplogic.xbee.api.XBeeTimeoutException;
 import com.rapplogic.xbee.api.zigbee.ZNetRxIoSampleResponse;
-import com.rapplogic.xbee.api.zigbee.ZNetTxRequest;
 
 import at.ac.tuwien.auto.iotsys.commons.Connector;
 
 public class XBeeConnector implements Connector{
+	private static final Logger log = Logger.getLogger(XBeeConnector.class.getName());
 
 	String port;
 	int baudRate;
 	private XBee xbee;
 	
+	private final Hashtable<String, ArrayList<XBeeWatchdog>> watchDogs = new Hashtable<String, ArrayList<XBeeWatchdog>>();
+	
 	public XBeeConnector(String port, int baudRate) {
-		
-		//PropertyConfigurator.configure("log4j.properties");
 		this.port = port;
 		this.baudRate = baudRate;
 	}
@@ -38,11 +34,32 @@ public class XBeeConnector implements Connector{
 		xbee.addPacketListener(new PacketListener(){
 
 			@Override
-			public void processResponse(XBeeResponse arg0) {
-				System.out.println("XBeeResponse: " + arg0);
-				
-			}
-			
+			public void processResponse(XBeeResponse response) {
+				if(response.getApiId() == ApiId.ZNET_IO_SAMPLE_RESPONSE){
+					ZNetRxIoSampleResponse ioSample = (ZNetRxIoSampleResponse) response;
+					int[] addressArray = ioSample.getRemoteAddress64().getAddress();
+					StringBuffer hexAddress = new StringBuffer();
+					
+					for(int i=0; i<addressArray.length; i++){
+						hexAddress.append(String.format("%02x", addressArray[i]));
+					}
+					String source = hexAddress.toString();
+					log.finest("Received packet from: " + hexAddress);	
+					
+					synchronized (watchDogs) {
+
+						if (watchDogs.containsKey(source)) {
+							for (XBeeWatchdog dog : watchDogs
+									.get(source)) {
+								dog.notifyWatchDog(response);
+							}
+						}
+						else{
+							log.finest("There is no watchdog registered for " + hexAddress);
+						}
+					}
+				}
+			}	
 		});
 		
 	}
@@ -52,70 +69,15 @@ public class XBeeConnector implements Connector{
 		xbee.close();	
 	}
 	
-	public Integer getLightValue() throws XBeeTimeoutException, XBeeException{
-		XBeeResponse response = xbee.getResponse();
-		Integer lightValue = null;
-		
-		if(response.getApiId() == ApiId.ZNET_IO_SAMPLE_RESPONSE){
-				
-			ZNetRxIoSampleResponse ioSample = (ZNetRxIoSampleResponse) response;
-			int[] addressArray = ioSample.getRemoteAddress64().getAddress();
-			String[] hexAddress = new String[addressArray.length];
-			for(int i=0; i<addressArray.length; i++){
-				hexAddress[i] = String.format("%02x", addressArray[i]);
+	public void addWatchDog(String observation, XBeeWatchdog xbeeWatchdog) {
+		synchronized (watchDogs) {
+			if (!watchDogs.containsKey(observation)) {
+				watchDogs.put(observation,
+						new ArrayList<XBeeWatchdog>());
 			}
-			
-			lightValue = ioSample.getAnalog1();
+			log.finest("Adding watchdog for address "
+					+ observation);
+			watchDogs.get(observation).add(xbeeWatchdog);
 		}
-		return lightValue;
-	}
-	
-	public Integer getTemperatureValue() throws XBeeTimeoutException, XBeeException{
-		// get the Node discovery timeout
-		xbee.sendAsynchronous(new AtCommand("NT"));
-		
-		XBeeResponse response = xbee.getResponse();
-		
-		System.err.println("Response: "+response);
-		
-		ApiId apiId = response.getApiId();
-		if(apiId == ApiId.AT_COMMAND_QUEUE){
-			System.err.println("1");
-		}else if(apiId == ApiId.AT_COMMAND){
-			System.err.println("2");
-		}else if(apiId == ApiId.AT_RESPONSE){
-			System.err.println("3");
-		}
-		
-		//xbee.sendAsynchronous(new ZNetTxRequest(address64, payload));
-		//xbee.sendAsynchronous(new ZNetTxRequest(address64, payload));		
-//		int[] address = {0x00,0x13,0xa2,0x00,0x40,0x7c,0x17,0x15};
-		int[] address = {0x7e,0x00,0x04,0x08,0x01,0x4e,0x54,0x54};
-		
-		XBeeAddress64 address64 = new XBeeAddress64(address);
-		int[] payload = new int[8];
-		
-		try {
-			xbee.sendRequest(new ZNetTxRequest(address64, payload));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		response = xbee.getResponse();
-		Integer temperatureValue = new Integer(0);
-		
-		if(response.getApiId() == ApiId.ZNET_IO_SAMPLE_RESPONSE){
-				
-			ZNetRxIoSampleResponse ioSample = (ZNetRxIoSampleResponse) response;
-			int[] addressArray = ioSample.getRemoteAddress64().getAddress();
-			String[] hexAddress = new String[addressArray.length];
-			for(int i=0; i<addressArray.length; i++){
-				hexAddress[i] = String.format("%02x", addressArray[i]);
-			}
-			
-			temperatureValue = ioSample.getAnalog2();
-		}
-		return temperatureValue;
 	}
 }
