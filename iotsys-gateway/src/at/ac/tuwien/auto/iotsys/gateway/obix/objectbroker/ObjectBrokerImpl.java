4b013c8f9b3f4a95eb68e3d1df0c188b97ea43f3
markus jung85 <markus.jung85@gmail.com>
2013-09-06 17:35:41 +0900

    Merge fix.

--- iotsys-gateway/src/at/ac/tuwien/auto/iotsys/gateway/obix/objectbroker/ObjectBrokerImpl.java
+++ iotsys-gateway/src/at/ac/tuwien/auto/iotsys/gateway/obix/objectbroker/ObjectBrokerImpl.java
@@ -40,6 +40,7 @@ import java.util.logging.Logger;
 import obix.Contract;
 import obix.ContractRegistry;
 import obix.Err;
+import obix.List;
 import obix.Obj;
 import obix.Op;
 import obix.Ref;
@@ -52,6 +53,8 @@ import at.ac.tuwien.auto.iotsys.gateway.obix.objects.HistoryHelper;
 import at.ac.tuwien.auto.iotsys.gateway.obix.objects.WatchImpl;
 import at.ac.tuwien.auto.iotsys.gateway.obix.objects.WatchServiceImpl;
 import at.ac.tuwien.auto.iotsys.gateway.obix.objects.iot.general.impl.LobbyImpl;
+import at.ac.tuwien.auto.iotsys.gateway.obix.objects.iot.logic.BinaryOperation;
+import at.ac.tuwien.auto.iotsys.gateway.obix.objects.iot.logic.LogicBinaryOperation;
 import at.ac.tuwien.auto.iotsys.gateway.service.GroupCommHelper;
 
 public class ObjectBrokerImpl implements ObjectBroker {
