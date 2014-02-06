01b44d5e27506c6fead74f5d159ee0ae72411d77
markus jung85 <markus.jung85@gmail.com>
2014-02-06 08:58:02 +0100

    Minor bufix

--- iotsys-coap/src/at/ac/tuwien/auto/iotsys/gateway/connectors/coap/Test.java
+++ iotsys-coap/src/at/ac/tuwien/auto/iotsys/gateway/connectors/coap/Test.java
@@ -7,16 +7,16 @@ public class Test {
 	
 	public static void main(String[] args){
 
-		Double readDouble = connector.readDouble("coap://[aaaa::c30c:0:0:2]:5683/temp", "value", new ResponseHandler() {
-				public void handleResponse(Response response) {	
-					double temp = Double.parseDouble( CoapConnector.extractAttribute("real", "val",
-							response.getPayloadString().trim()));
-					
-									
-				}
-			});
-		
-		System.out.println("Temp is: " + readDouble);
+//		Double readDouble = connector.readDouble("coap://[aaaa::c30c:0:0:2]:5683/temp", "value", new ResponseHandler() {
+//				public void handleResponse(Response response) {	
+//					double temp = Double.parseDouble( CoapConnector.extractAttribute("real", "val",
+//							response.getPayloadString().trim()));
+//					
+//									
+//				}
+//			});
+//		
+//		System.out.println("Temp is: " + readDouble);
 	}
 
 }
