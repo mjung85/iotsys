1cca24e774cd22971d21a0437f14c08a3fea812a
Markus Jung <markus.jung85@gmail.com>
2014-04-15 17:00:21 +0200

    IoTSyS Device interface

--- iotsys-common/src/at/ac/tuwien/auto/iotsys/commons/obix/objects/iot/IoTSySDevice.java
+++ iotsys-common/src/at/ac/tuwien/auto/iotsys/commons/obix/objects/iot/IoTSySDevice.java
@@ -1,5 +1,6 @@
 package at.ac.tuwien.auto.iotsys.commons.obix.objects.iot;
 
-public interface Addressable {
+public interface IoTSySDevice {
 	public String getBusAddress();
+	public boolean forwardGroupAddress();
 }
