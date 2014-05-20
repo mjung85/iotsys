3aa693c4871924a0035538f096a2000220b499c7
Markus Jung <markus.jung85@gmail.com>
2014-05-20 03:33:34 +0200

    IPv6 multicast forwarder

---
+++ iotsys-mcast-forwarder/src/MCastForwarder.java
@@ -0,0 +1,238 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Institute of Computer Aided Automation, Automation Systems
+ * Group, TU Wien. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer. 2. Redistributions in
+ * binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. 3. Neither the name of the
+ * Institute nor the names of its contributors may be used to endorse or promote
+ * products derived from this software without specific prior written
+ * permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * This file is part of the IoTSyS project.
+ ******************************************************************************/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.jnetpcap.Pcap;
+import org.jnetpcap.PcapIf;
+import org.jnetpcap.packet.JRegistry;
+import org.jnetpcap.packet.PcapPacket;
+import org.jnetpcap.packet.PcapPacketHandler;
+import org.jnetpcap.protocol.network.Ip6;
+import org.jnetpcap.protocol.tcpip.Udp;
+
+public class MCastForwarder {
+	private HashSet<String> interfaces = null;
+	private Logger log = Logger.getLogger(MCastForwarder.class.getName());
+	private int port = 5683;
+
+	// specify interfaces between which multicasts should be forwarded through
+	// command line args, e.g. eth0, tun0, ...
+	public static void main(String[] args) {
+		HashSet<String> interfaces = new HashSet<String>();
+		for(int i = 0; i < args.length; i++){
+			interfaces.add(args[i]);
+		}
+		MCastForwarder mcastForwarder = new MCastForwarder(interfaces);
+		mcastForwarder.startForwarding();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			reader.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public MCastForwarder(HashSet<String> interfaces) {
+		this.interfaces = interfaces;
+	}
+
+	public void startForwarding() {
+		List<PcapIf> alldevs = new ArrayList<PcapIf>();
+		StringBuilder errbuf = new StringBuilder();
+
+		int r = Pcap.findAllDevs(alldevs, errbuf);
+		if (r == Pcap.NOT_OK || alldevs.isEmpty()) {
+			log.info("No devs found");
+			return;
+		}
+
+		int i = 0;
+		int pick = 0;
+
+		for (PcapIf device : alldevs) {
+			String description = (device.getDescription() != null) ? device
+					.getDescription() : "No description available.";
+			log.info("" + (i++) + "#: " + device.getName() + " " + description);
+			
+			if (interfaces.contains(device.getName())) {
+				pick = i - 1;
+				startCaptureForInterface(alldevs, pick, device.getName());
+			}
+		
+		}	
+	}
+	
+	public synchronized void forward(Inet6Address dest, Inet6Address src, byte[] payload){
+		log.info("Forward " + dest.toString() + "," + src.toString() + " " + payload.length + " bytes." );
+	}
+	
+	private Pcap startCaptureForInterface(List<PcapIf> alldevs, int pick, final String ifName){
+		log.info("openening device for pcap: " + alldevs.get(pick).getName());
+		PcapIf device = alldevs.get(pick);
+
+		int snaplen = 64 * 1024;
+		int flags = Pcap.MODE_PROMISCUOUS;
+		int timeout = 10 * 1000;
+		StringBuilder errbuf = new StringBuilder();
+		final Pcap pcap = Pcap.openLive(device.getName(), snaplen, flags,
+				timeout, errbuf);
+
+		if (pcap == null) {
+			log.info("Cannot listen.");
+		}
+		final MCastHandler<String> mcastHandler = new MCastHandler<String>(port, pcap, this, device.getName());
+		
+		log.info("Registered mcastHandler handler.");
+
+		Thread packetlistener = new Thread() {
+
+			@Override
+			public void run() {
+				pcap.loop(0, mcastHandler, "McastHandler - " + ifName);
+			}
+
+		};
+		
+		packetlistener.setDaemon(true);
+		packetlistener.start();
+		return pcap;
+	}
+}
+
+class MCastHandler<String> implements PcapPacketHandler<String> {
+	private int port = 5683;
+	private static final Logger log = Logger
+			.getLogger(MCastHandler.class.getName());
+
+	private Pcap pcap = null;
+	
+	private MCastForwarder mcastForwarder = null;
+	private String devName;
+
+	public MCastHandler(int port, Pcap pcap, MCastForwarder mcastForwarder, String devName) {
+		if (port != 0)
+			this.port = port;
+
+		this.pcap = pcap;
+		this.mcastForwarder = mcastForwarder;
+		this.devName = devName;
+	}
+
+	public void nextPacket(PcapPacket packet, String user) {
+		System.out.println("Next packet on " + devName);
+		if(devName.equals("tun0")){
+			System.out.println("break");
+		}
+	
+		if (packet.getHeaderCount() == 1) { // indication for tunnel packet
+			log.info("Packet received through PCAP (tunnel packet).");
+			// the payload might be directly an ipv6 packet
+			if (packet.getByte(0) >> 4 == 6 && packet.size() > 48) { // ipv6
+																		// version,
+																		// at
+																		// least
+																		// ipv6
+																		// and
+																		// udp
+																		// header
+				byte[] srcAddressByte = new byte[16];
+				packet.getByteArray(8, srcAddressByte);
+				byte[] destAddress = new byte[16];
+				byte[] payload = new byte[packet.size() - 48]; // everything
+																// beside IPv6
+																// and UDP
+																// header
+				packet.getByteArray(24, destAddress);
+				try {
+					Inet6Address srcIpv6 = (Inet6Address) Inet6Address
+							.getByAddress(srcAddressByte);
+					Inet6Address destIpv6 = (Inet6Address) Inet6Address
+							.getByAddress(destAddress);
+					// extract udp header (4 byte)
+					byte[] destPortBytes = new byte[2];
+					packet.getByteArray(42, destPortBytes);
+
+					byte[] srcPortBytes = new byte[2];
+					packet.getByteArray(40, destPortBytes);
+
+					Integer srcPort = srcPortBytes[0] * 256 + srcPortBytes[1];
+					Integer destPort = destPortBytes[0] * 256
+							+ destPortBytes[1];
+
+					packet.getByteArray(48, payload);
+
+					if (destPort == this.port && destIpv6.isMulticastAddress()) {
+
+						log.info("Received multicast message through PCAP (over tunnel adapter)s.");
+						mcastForwarder.forward(destIpv6,srcIpv6, payload);			
+					}
+
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		int idDLT = JRegistry.mapDLTToId(pcap.datalink());
+		packet.scan(idDLT);
+		if (packet.hasHeader(Udp.ID) && packet.hasHeader(Ip6.ID)) {
+			System.out.println("UDP and IPv6");
+			Ip6 ipv6 = packet.getHeader(new Ip6());
+			Udp udp = packet.getHeader(new Udp());
+			byte[] destination = ipv6.destination();
+			Inet6Address dest = null;
+			Inet6Address src = null;
+			
+			try {
+				dest = (Inet6Address) InetAddress.getByAddress(destination);
+				src = (Inet6Address) InetAddress.getByAddress(ipv6.source());
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			if (udp.destination() == this.port && dest.isMulticastAddress()) {
+				log.info("Received multicast message through PCAP.");
+				mcastForwarder.forward(dest, src,udp.getPayload());			
+			}
+		}
+	}	
+}
