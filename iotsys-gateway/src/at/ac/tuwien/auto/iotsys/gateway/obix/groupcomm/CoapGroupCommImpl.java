f0742f472978355e852a9ebda2d301a0a6ca6e5d
Markus Jung <markus.jung85@gmail.com>
2014-05-22 22:00:52 +0200

    Multicast fix

--- iotsys-gateway/src/at/ac/tuwien/auto/iotsys/gateway/obix/groupcomm/CoapGroupCommImpl.java
+++ iotsys-gateway/src/at/ac/tuwien/auto/iotsys/gateway/obix/groupcomm/CoapGroupCommImpl.java
@@ -17,6 +17,7 @@ import obix.Obj;
 import obix.Str;
 import at.ac.tuwien.auto.iotsys.commons.obix.objects.iot.IoTSySDevice;
 import at.ac.tuwien.auto.iotsys.gateway.service.GroupCommService;
+import at.ac.tuwien.auto.iotsys.gateway.service.impl.GroupCommServiceImpl;
 
 
 public class CoapGroupCommImpl extends GroupCommImpl{
@@ -39,6 +40,10 @@ public class CoapGroupCommImpl extends GroupCommImpl{
 				String formatted = String.format("%04X:%04X:%04X:%04X:%04X:%04X:%04X:%04X", (((short) inet6.getAddress()[0]) *256 + inet6.getAddress()[1]) & 0XFFFF, (((short) inet6.getAddress()[2]) *256 + inet6.getAddress()[3]) & 0XFFFF, (((short) inet6.getAddress()[4]) *256 + inet6.getAddress()[5]) & 0XFFFF,
 																							(((short) inet6.getAddress()[6]) *256 + inet6.getAddress()[7]) & 0XFFFF, (((short) inet6.getAddress()[8]) *256 + inet6.getAddress()[9]) & 0XFFFF, (((short) inet6.getAddress()[10]) *256 + inet6.getAddress()[11]) & 0XFFFF,
 																							(((short) inet6.getAddress()[12]) *256 + inet6.getAddress()[13]) & 0XFFFF, (((short) inet6.getAddress()[14]) *256 + inet6.getAddress()[15]) & 0XFFFF  );
+				
+				
+				GroupCommServiceImpl.getInstance().registerAsReceiver(inet6, this.datapoint);
+				
 				String payload = "<str val=\"" + formatted + "\">";
 				
 				Request request = new POSTRequest();
@@ -46,11 +51,12 @@ public class CoapGroupCommImpl extends GroupCommImpl{
 				request.setPayload(payload);
 				request.setType(messageType.CON);
 				request.setOption(new Option(MediaTypeRegistry.APPLICATION_XML,OptionNumberRegistry.ACCEPT));
+				request.setOption(new Option(16, OptionNumberRegistry.SIZE));
 			
 				// specify URI of target endpoint
 				request.setURI(busUri);
 				// enable response queue for blocking I/O
-				request.enableResponseQueue(true);
+				request.enableResponseQueue(true);						
 				
 				try {
 					request.execute();
@@ -93,7 +99,7 @@ public class CoapGroupCommImpl extends GroupCommImpl{
 																							(((short) inet6.getAddress()[6]) *256 + inet6.getAddress()[7]) & 0XFFFF, (((short) inet6.getAddress()[8]) *256 + inet6.getAddress()[9]) & 0XFFFF, (((short) inet6.getAddress()[10]) *256 + inet6.getAddress()[11]) & 0XFFFF,
 																							(((short) inet6.getAddress()[12]) *256 + inet6.getAddress()[13]) & 0XFFFF, (((short) inet6.getAddress()[14]) *256 + inet6.getAddress()[15]) & 0XFFFF  );
 				String payload = "<str val=\"" + formatted + "\">";
-				
+				GroupCommServiceImpl.getInstance().unregisterReceiverObject(inet6, this.datapoint);
 				Request request = new POSTRequest();
 				System.out.println("Adresse: " + tempUri + "\nPayload: " + payload);
 			
